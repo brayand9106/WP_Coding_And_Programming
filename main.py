9f5160940cd02dc83a38c0971889151e59b112e5
Brayan <brayand9106@gmail.com>
2024-11-16 11:42:25 -0700

    initialized main ctk window

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+#####NOTE: Make sure to run "python main.py" to run the program using a virtual env#####
+#####NOTE: if program still doesnt work, go to activate file in your venv to add export TCL_LIBRARY="C:\Users\Braya\AppData\Local\Programs\Python\Python313\tcl\tcl8.6" to fix tlc error#####
+
+import customtkinter as ctk
+
 if __name__ == "__main__":
     print("Main Executed")
+
+    app = ctk.CTk()
+    app.title("FinanceTBD")
+    app.geometry("1200x700")
+
+
+    app.mainloop()
