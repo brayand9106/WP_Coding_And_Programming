4cfb77557f4ac2917eb83d0c98815bd27f964660
Brayan <brayand9106@gmail.com>
2024-11-18 22:09:19 -0700

    WIP Sidebar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #####NOTE: if program still doesnt work, go to activate file in your venv to add export TCL_LIBRARY="C:\Users\Braya\AppData\Local\Programs\Python\Python313\tcl\tcl8.6" to fix tlc error#####
 
 import customtkinter as ctk
+from Frames.Sidebar_Frame import SideBarFrame
 
 if __name__ == "__main__":
     print("Main Executed")
@@ -10,5 +11,7 @@ if __name__ == "__main__":
     app.title("FinanceTBD")
     app.geometry("1200x700")
 
+    Sidebar = SideBarFrame(app, "Statistics", "Settings", "TBD", "TBD2", "TBD3")
+    Sidebar.grid()
 
     app.mainloop()
