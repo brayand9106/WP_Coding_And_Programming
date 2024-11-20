f0e057269d6687998be70f5ef2436230673054bd
Brayan <brayand9106@gmail.com>
2024-11-20 13:04:04 -0700

    Completed Sidebar

--- Frames/Sidebar_Frame.py
+++ Frames/Sidebar_Frame.py
@@ -15,20 +15,22 @@ class SideBarFrame(ctk.CTkFrame):
         self.grid_columnconfigure(0, weight=1)  # Ensure buttons fill the width
         self.grid_rowconfigure("all", weight=1)  # Equal weight for rows
 
-        def onClick(button_name):
-            print(button_name + " clicked on!")
-
-        def toggle_visibility(self):
-            if self.winfo_ismapped():  # Check if the frame is visible
-                self.grid_remove()  # Hide the frame
-            else:
-                self.grid()
-
         for i, nav in enumerate(navs):
-            self.button = ctk.CTkButton(self, text=str(nav), command=lambda name=nav: onClick(name))
+            self.button = ctk.CTkButton(self, text=str(nav), command=lambda name=nav: self.onClick(name))
             self.navList.append(self.button)
             print(str(self.button))
             self.button.grid(row=i, pady=30, padx=10, sticky="ew")
+
+
+    def onClick(self, button_name):
+        print(f"{button_name} clicked on")
+
+    def toggle_visibility(self):
+        if self.winfo_ismapped():  # Check if the frame is visible
+            self.grid_remove()  # Hide the frame
+        else:
+            self.grid()
+            
             
 
 
