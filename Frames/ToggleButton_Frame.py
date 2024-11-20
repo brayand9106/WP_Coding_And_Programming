276b18d373f031a9ae8c6456ca31e4aa857a8188
Brayan <brayand9106@gmail.com>
2024-11-20 09:23:40 -0700

    Fixed toggle button styling

--- Frames/ToggleButton_Frame.py
+++ Frames/ToggleButton_Frame.py
@@ -5,12 +5,13 @@ This frame creates a toggle button that when clicked it toggles the
 visibility of the sidebar frame
 '''
 
-class ToggleButtonFrame(ctk.CTkFrame()):
+class ToggleButtonFrame(ctk.CTkFrame):
     def __init__(self, master, target_frame):
         super().__init__(master)
-        self.button = ctk.CTkButton(master, text="<")
+        self.button = ctk.CTkButton(self, text="<", width=25, command=lambda frame=target_frame: onClick(frame))
+        self.button.grid(sticky="nw")
 
         def onClick(target_frame):
-            pass
+            target_frame.toggle_visibility(target_frame)
 
 
