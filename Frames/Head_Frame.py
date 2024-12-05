715895591c68b2164654a956484fb9595ee1fd03
Brayan <brayand9106@gmail.com>
2024-12-05 14:49:22 -0700

    WIP head frame

--- Frames/Head_Frame.py
+++ Frames/Head_Frame.py
@@ -1,8 +1,16 @@
+from PIL import ImageTk
+
+
 import customtkinter as ctk
 
 class HeadFrame(ctk.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, img):
         super().__init__(master)
         
         self.grid_columnconfigure(0, weight=1)  # Ensure buttons fill the width
         self.grid_rowconfigure("all", weight=1)  # Equal weight for rows
+
+        ctk_logo = ImageTk.PhotoImage(img)
+
+        logo = ctk.CTkLabel(self, image=ctk_logo, text="")
+
