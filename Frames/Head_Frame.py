6915f610392d4e00c1062f6e53f6926c92b59eea
Brayan <brayand9106@gmail.com>
2025-01-13 16:51:51 -0700

    made home button from logo

--- Frames/Head_Frame.py
+++ Frames/Head_Frame.py
@@ -1,10 +1,8 @@
 from PIL import ImageTk
-
-
 import customtkinter as ctk
 
 class HeadFrame(ctk.CTkFrame):
-    def __init__(self, master, img):
+    def __init__(self, master, img, home_command):
         super().__init__(master)
         
         self.configure(
@@ -20,7 +18,8 @@ class HeadFrame(ctk.CTkFrame):
         # Create a nested frame
         nested_frame = ctk.CTkFrame(self, fg_color="transparent", bg_color="transparent")
         nested_frame.grid(sticky="w", padx=2, pady=2)
-        # Add the logo to the nested frame
-        self.logo = ctk.CTkLabel(nested_frame, image=ctk_logo, text="", fg_color="transparent", bg_color="transparent")
-        self.logo.image = ctk_logo  # Keep a reference to avoid garbage collection
-        self.logo.grid(sticky="nwe", padx=0, pady=0)
+        
+        # Add the logo as an image button to the nested frame
+        self.logo_button = ctk.CTkButton(nested_frame, image=ctk_logo, text="", fg_color="transparent", bg_color="transparent", hover_color=nested_frame.cget("bg_color"), command=home_command)
+        self.logo_button.image = ctk_logo  # Keep a reference to avoid garbage collection
+        self.logo_button.grid(sticky="nwe", padx=0, pady=0)
